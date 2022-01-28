21d3caeed052038376fda1aaa383e113c83f45f7
ben-roomberg <ben.roomberg@gmail.com>
2022-01-28 12:39:00 -0500

    comment changes

--- Metrics/Controllers/MetricSelectController.cs
+++ Metrics/Controllers/MetricSelectController.cs
@@ -7,7 +7,6 @@ using Metrics.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Metrics.Controllers
 {
